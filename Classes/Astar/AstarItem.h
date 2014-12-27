4ed7287c31b4da889c212c07db204756d77ba91c
Jingz <455786463@qq.com>
2014-12-27 20:02:56 +0800

    move the codes abour F attributes of Item

--- Classes/Astar/AstarItem.h
+++ Classes/Astar/AstarItem.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+Copyright (c) 2014-2015 Jingz @ ReccaStudio.
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+
 #pragma once
 
 #include "cocos2d.h"
@@ -5,14 +29,14 @@
 
 //#define Jingz_Debug
 
-extern struct AstarCompare;
+extern struct AstarLessThan;
 extern class AstarMapManager; 
 
 
 
 class AstarItem :public cocos2d::Ref
 {
-	friend AstarCompare;
+	friend AstarLessThan;
 	friend AstarMapManager;
 
 protected:
@@ -150,23 +174,23 @@ private:
 	
 	int m_iRow;
 	int m_iCol;
-	
+	float m_fF;
 	bool m_bIsVisited;
 
 	float m_fG;
 	float m_fH;
-	float m_fF;
+	
 
 	AstarItem * m_pParentItem;//TMXmap中地图数据是id，所以不需要再使用链表指针
 
 };
 
-struct AstarCompare:public std::binary_function<AstarItem*,AstarItem*,bool>
+struct AstarLessThan:public std::binary_function<AstarItem*,AstarItem*,bool>
 {
 public:
 	bool operator()(const AstarItem* left,const AstarItem * right)const
 	{
-		if (left->getF()-right->getF()<1E-5)
+		if (std::abs(left->getF()-right->getF()) < 1E-5 )
 		{
 			//同耗散，优先考虑预测值
 			return left->getH()<right->getH();
